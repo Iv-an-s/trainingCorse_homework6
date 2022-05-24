a4c2c1bfa7e5f3f2bedbc31c2d9da4819748eb63
Ivan <ivan_semenov@bk.ru>
2022-05-25 00:27:08 +0300

    homework_6

--- src/main/java/ru/geekbrains/isemenov/app/HwServlet.java
+++ src/main/java/ru/geekbrains/isemenov/app/HwServlet.java
@@ -1,4 +1,29 @@
 package ru.geekbrains.isemenov.app;
 
-public class HwServlet {
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HwServlet", urlPatterns = "/hw_servlet")
+public class HwServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(HwServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Log: GET-request received");
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().printf("<html><body>");
+
+        resp.getWriter().printf("<h1>Homework #6 well done!</h1>");
+
+        resp.getWriter().printf("</body></html>");
+        resp.getWriter().close();
+    }
 }
